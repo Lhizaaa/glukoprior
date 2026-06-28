d62998dc1f3f5d6203fff641c902a4b52997e432
Lhizaaa <kimzi2549@gmail.com>
2026-06-28 09:42:19 +0700

    Update Code

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ function go(pg){
   document.querySelectorAll('.page').forEach(p=>p.classList.remove('on'));
   document.getElementById('pg-'+pg).classList.add('on');
   document.querySelectorAll('#nav a').forEach(a=>a.classList.toggle('on',a.dataset.pg===pg));
+  document.querySelectorAll('#mnav [data-pg]').forEach(b=>b.classList.toggle('on',b.dataset.pg===pg));
   window.scrollTo({top:0,behavior:'smooth'});
 }
 function renderAll(includeData=true){
@@ -11,6 +12,7 @@ function renderAll(includeData=true){
 }
 
 document.querySelectorAll('#nav a').forEach(a=>a.onclick=()=>go(a.dataset.pg));
+document.querySelectorAll('#mnav [data-pg]').forEach(b=>b.onclick=()=>go(b.dataset.pg));
 document.querySelectorAll('#methodTabs .mtab').forEach(b=>b.onclick=()=>{
   curMethod=b.dataset.m;
   document.querySelectorAll('#methodTabs .mtab').forEach(x=>x.classList.toggle('on',x===b));
